87660ab76e79c6d6c3ce2ff73c9b381c84b4a9c3
Carlos96999 <54123771+Carlos96999@users.noreply.github.com>
2019-10-03 07:18:39 -0500

    Add files via upload

--- Clásico_a_cuántico.py
+++ Clásico_a_cuántico.py
@@ -1,19 +1,40 @@
 import Librería_vectores_matrices
 
-def Canicas(matriz, vector):
+def Canicas(matriz, vector, click):
 
     if len(matriz) == len(vector):
-        respuesta = Librería_vectores_matrices.AccionMatrizVector(matriz,vector)
 
-        for i in range(len(respuesta)):
+        respuesta = vector
+        while click != 0:
+            
+            respuesta = Librería_vectores_matrices.AccionMatrizVector(matriz,respuesta)
+            click -= 1
 
-            if respuesta[i][0] != 0:
-                respuesta[i][0] = True
-                respuesta[i][1] = True
-            else:
-                respuesta[i][0] = False
-                respuesta[i][1] = False
+        return respuesta
+
+##        for i in range(len(respuesta)):
+##
+##            if respuesta[i][0] != 0:
+##                respuesta[i][0] = True
+##                respuesta[i][1] = True
+##            else:
+##                respuesta[i][0] = False
+##                respuesta[i][1] = False
     else:
         print("Dimensiones incorrectas. Por favor digita de nuevo")
 
+def Probabilidad(matriz, vector):
+
+    respuesta = vector
+    
+    if len(matriz) == len(vector):        
+        respuesta = Librería_vectores_matrices.AccionMatrizVector(matriz,respuesta)
+        return respuesta
+    else:
+        print("Dimensiones incorrectas. Por favor digita de nuevo")
+
+def DobleRendija(matriz):
+
+    respuesta = matrizmultiplication(matriz,matriz)
+    return respuesta
 
